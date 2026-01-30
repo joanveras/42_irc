6ae4b368d14f62449ad62c4c68a7d0dba1dcc23d
aneri-da <alesssandro.soares1604@gmail.com>
2026-01-30 15:04:51 -0300

    feat: add Channel class interface with the methods and attributes

---
+++ include/Channel.hpp
@@ -0,0 +1,68 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include "Client.hpp"
+#include <string>
+#include <vector>
+#include <map>
+
+class Channel {
+    private:
+        std::string _name;
+        std::string _topic;
+        std::string _key;
+        std::size_t _limit;
+
+        std::map<int, Client*> _members;
+        std::vector<int> _operators;
+        std::vector<int> _invitedFds;
+
+        bool _modeI;// invite only
+        bool _modeT;// topic restricted to operators
+        bool _modeK;// key required
+        bool _modeL;// user limit active
+
+    private:
+        bool isValidName(const std::string &name) const ;
+
+    public:
+        //canonical orthodox form
+        Channel(const std::string &name);
+        ~Channel();
+        Channel(const Channel &other);
+        Channel &operator=(const Channel &other);
+
+        bool isMember(int fd) const ;
+        bool isOperator(int fd) const ;
+        bool isInviteOnly() const ;
+        bool isTopicRestricted() const ;
+        bool hasKey() const ;
+        bool isFull() const ;
+
+        //setters
+        void setName(const std::string &name);
+        void setTopic(const std::string &topic);
+        void setKey(const std::string &key);
+        void setLimit(std::size_t limit);
+
+        //getters
+        std::size_t getLimit() const ;
+        const std::string &getKey() const ;
+        const std::string &getTopic() const ;
+        const std::string &getName() const ;
+
+        //member management
+        void addClient(Client *client);
+        void removeClient(int fd);
+        void addOperator(int fd);
+        void removeOperator(int fd);
+
+        // mode management
+        void setMode(char mode, bool setting);
+        bool getMode(char mode) const ;
+
+        //communication
+        void broadcast(const std::string &message, int exclude_fd);// enviar mensagem para todos no canal
+};
+
+#endif
