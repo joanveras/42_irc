9b30a4a262ba8aa50ec80f5bebe10ae9845e10fb
aneri-da <alesssandro.soares1604@gmail.com>
2026-02-02 16:41:43 -0300

    feat: add default constructor, cleaning correctly the object in the destructor, copy correctly in the copy assignment constructor, check the limit and the key of a channel before add a member in the addMember method, remove the check of the size of the channel in the addOperator method, remove the call of the removeMember in the removeOperator method it is unnecessary, add setMode implementation, add getMode implementation, add broadcast implementation

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,8 +1,26 @@
 #include "../include/Channel.hpp"
 #include <algorithm>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+Channel::Channel() {
+    _name = "";
+    _topic = "";
+    _key = "";
+    _limit = 0;
+    _modeI = false;
+    _modeT = false;
+    _modeK = false;
+    _modeL = false;
+}
+
 
 Channel::Channel(const std::string &name) {
     _name = name;
+    _topic = "";
+    _key = "";
+    _limit = 0;
     _modeI = false;
     _modeT = false;
     _modeK = false;
@@ -10,7 +28,9 @@ Channel::Channel(const std::string &name) {
 }
 
 Channel::~Channel() {
-
+    _members.clear();
+    _operators.clear();
+    _invitedFds.clear();
 }
 
 Channel::Channel(const Channel &other) {
@@ -19,7 +39,17 @@ Channel::Channel(const Channel &other) {
 
 Channel &Channel::operator=(const Channel &other) {
     if (this != &other) {
-
+        this->_name = other._name;
+        this->_topic = other._topic;
+        this->_key = other._key;
+        this->_limit = other._limit;
+        this->_modeI = other._modeI;
+        this->_modeT = other._modeT;
+        this->_modeK = other._modeK;
+        this->_modeL = other._modeL;
+        this->_members = other._members;
+        this->_operators = other._operators;
+        this->_invitedFds = other._invitedFds;
     }
     return *this;
 }
@@ -80,29 +110,16 @@ bool Channel::isInviteOnly() const {
     return _modeI;
 }
 
-//Essa validação tem que entrar no server para não instanciar o objeto no caso de nome inválido
-bool Channel::isValidName(const std::string &name) const {
-    if (name.length() > 200) {
-        return false;
-    }
-    if (name[0] != '&' && name[0] != '#') {
-        return false;
-    }
-    for (std::string::const_iterator it = name.begin(); it != name.end(); it++) {
-        if (*it == ' ' || *it == ',' || *it == '\a') {
-            return false;
-        }
-    }
-    return true;
-}
-
-void Channel::addMember(Client *client) {
-    if (_members.size() < _limit) {
-        _members.insert(std::pair<int, Client*>(client->getFd(),client));
-    }
-    else {
+void Channel::addMember(Client *client, std::string &givenKey) {
+    if (_modeL && _members.size() >= _limit) {
         //enviar mensagem em conformidade com RFC 1459
+        return;
     }
+    if (_modeK && _key != givenKey) {
+        //enviar mensagem em conformidade com RFC 1459
+        return;
+    }
+    _members.insert(std::pair<int, Client*>(client->getFd(),client));
 }
 
 void Channel::removeMember(int clientFd) {
@@ -117,25 +134,58 @@ void Channel::removeMember(int clientFd) {
 }
 
 void Channel::addOperator(int clientFd) {
-    if (_members.size() < _limit) {
-        _operators.push_back(clientFd);
-    }
-    else {
-        //enviar mensagem em conformidade com RFC 1459
-    }
+    _operators.push_back(clientFd);
 }
 
 void Channel::removeOperator(int clientFd) {
     std::vector<int>::iterator it = std::find(_operators.begin(), _operators.end(), clientFd);
     if (it != _operators.end()) {
         _operators.erase(it);
-        this->removeMember(clientFd);
     }
     else {
         //enviar mensagem em conformidade com RFC 1459
     }
 }
 
-void Channel::broadcast(const std::string &message, int excludeFd) {
+void Channel::setMode(char mode, bool setting) {
+    switch (mode) {
+        case 'i': 
+            _modeI = setting;
+            break;
+        case 't':
+            _modeT = setting;
+            break;
+        case 'k':
+            _modeK = setting;
+            break;
+        case 'l':
+            _modeL = setting;
+            break;
+        default:
+            break;
+    }
+}
 
+bool Channel::getMode(char mode) const {
+    if (mode == 'i') {
+        return _modeI;
+    }
+    else if (mode == 't') {
+        return _modeT;
+    }
+    else if (mode == 'k') {
+        return _modeK;
+    }
+    else if (mode == 'l') {
+        return _modeL;
+    }
+    return false;
+}
+
+void Channel::broadcast(const std::string &message, int excludeFd) {
+    for (std::map<int, Client*>::iterator it = _members.begin(); it != _members.end(); it++) {
+        if(it->first != excludeFd) {
+            send(it->first, message.c_str(), message.length(), 0);
+        }
+    }
 }
