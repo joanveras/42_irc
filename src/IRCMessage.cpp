72d436f5fa0238d722c12972996bf7e22a3fb80d
Nathan-N7 <nathan.rod1710@gmail.com>
2026-02-11 14:54:22 -0300

    tratative prefix empty

--- src/IRCMessage.cpp
+++ src/IRCMessage.cpp
@@ -52,7 +52,7 @@ bool IRCMessage::parse(const std::string &raw) {
     line.erase(line.size() - IRC_PARAM_OFFSET);
 
   size_t pos = 0;
-  while (pos < line.length() && std::isspace(line[pos]))
+  while (pos < line.length() && line[pos] == ' ')
     pos++;
   if (pos >= line.length())
     return false;
@@ -64,6 +64,8 @@ bool IRCMessage::parse(const std::string &raw) {
       return false;
 
     _prefix = line.substr(pos + IRC_PARAM_OFFSET, end - pos - IRC_PARAM_OFFSET);
+    if (_prefix.empty())
+      return false;
     pos = end + IRC_PARAM_OFFSET;
     while (pos < line.length() && line[pos] == ' ')
       pos++;
